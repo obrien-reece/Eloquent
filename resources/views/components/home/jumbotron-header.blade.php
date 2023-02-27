f6f6cdc0ac9447afa000c515ac04b7cd611ce872
OBrien-reece <incehc.evans.o@gmail.com>
2023-02-27 10:08:08 +0300

    added search box to jumbotron

--- resources/views/components/home/jumbotron-header.blade.php
+++ resources/views/components/home/jumbotron-header.blade.php
@@ -1,12 +1,33 @@
-<div class="jumbotron">
-    <h1 class="display-4">Welcome to Lutrix</h1>
-    <p class="lead">We are a movie info website that aims to provide all the relevant information that is concerned with movies on demand</p>
-{{--    <hr class="my-4">--}}
-    <p>If you want the most comprehensive information on movies, here is the place</p>
-</div>
-<br>
-<div class="text-center">
-    <span class="display-6" style="font-family: 'Luxurious Roman', cursive;">
-        {{ $slot }}
-    </span>
+<div class="container">
+    <di class="row">
+        <div class="col-md-8">
+            <div class="jumbotron">
+                <h1 class="display-4">Welcome to Lutrix</h1>
+                <p class="lead">We are a movie info website that aims to provide all the relevant information that is concerned with movies on demand</p>
+                {{--    <hr class="my-4">--}}
+                <p>If you want the most comprehensive information on movies, here is the place</p>
+            </div>
+            <br>
+            <div class="text-center">
+                <span class="display-6" style="font-family: 'Luxurious Roman', cursive;">
+                    {{ $slot }}
+                </span>
+            </div>
+
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h3>Find the movie you are looking for <u>without any hustle</u></h3>
+                    <form action="#" method="GET">
+                        <div class="form-group">
+                            <label for=""><small>Search for movie studio, name, actor, description</small></label>
+                            <input type="text" name="search" class="form-control mb-2" placeholder="Search for..."></input>
+                            <button type="submit" name="searchbtn" class="btn btn-dark btn-block">Search from countless movies</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </di>
 </div>
