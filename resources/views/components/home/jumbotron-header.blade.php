d77c4ff606b8d7a4ec94ed4f8f8e34d8b071b637
OBrien-reece <indeche.evans.o@gmail.com>
2023-03-06 17:53:40 +0300

    Edited jumbotron

--- resources/views/components/home/jumbotron-header.blade.php
+++ resources/views/components/home/jumbotron-header.blade.php
@@ -5,7 +5,7 @@
     <di class="row">
         <div class="col-md-8">
             <div class="jumbotron">
-                <h1 class="display-4">Welcome to Lutrix</h1>
+                <h1 class="display-4">Welcome to EloquentLutrix</h1>
                 <p class="lead">We are a movie info website that aims to provide all the relevant information that is concerned with movies on demand</p>
                 {{--    <hr class="my-4">--}}
                 <p>If you want the most comprehensive information on movies, here is the place</p>
